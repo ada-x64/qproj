94d83ec9d4b7067a2f33381ad2b95b7d9ecfcff8
phoenix mandala <phoenix.mandala@pm.me>
2025-04-02 21:25:07 -0500

    fixup rust-analyzer

--- crates/inspector/src/lib.rs
+++ crates/inspector/src/lib.rs
@@ -19,26 +19,25 @@ pub struct InspectorPlugin;
 impl Plugin for InspectorPlugin {
     fn build(&self, app: &mut App) {
         let assets = app.world_mut().load_asset("inspector");
-        app.add_plugins((
-            DefaultInspectorConfigPlugin,
-            bevy_egui::EguiPlugin,
-        ))
-        .init_resource::<DockState>()
-        .insert_resource(UiState::new(assets))
-        .add_systems(Startup, (spawn_camera, spawn_ui))
-        .add_systems(
-            Update,
-            (Dolly::<InspectorCam>::update_active, update_camera),
-        )
-        .add_systems(FixedUpdate, pause_time)
-        .add_systems(
-            PostUpdate,
-            show_ui_system
-                .before(EguiPostUpdateSet::ProcessOutput)
-                .before(bevy_egui::end_pass_system)
-                .before(bevy::transform::TransformSystem::TransformPropagate),
-        )
-        .add_systems(PostUpdate, set_camera_viewport.after(show_ui_system));
+        app.add_plugins((DefaultInspectorConfigPlugin, bevy_egui::EguiPlugin))
+            .init_resource::<DockState>()
+            .insert_resource(UiState::new(assets))
+            .add_systems(Startup, (spawn_camera, spawn_ui))
+            .add_systems(
+                Update,
+                (Dolly::<InspectorCam>::update_active, update_camera),
+            )
+            .add_systems(FixedUpdate, pause_time)
+            .add_systems(
+                PostUpdate,
+                show_ui_system
+                    .before(EguiPostUpdateSet::ProcessOutput)
+                    .before(bevy_egui::end_pass_system)
+                    .before(
+                        bevy::transform::TransformSystem::TransformPropagate,
+                    ),
+            )
+            .add_systems(PostUpdate, set_camera_viewport.after(show_ui_system));
     }
 }
 
