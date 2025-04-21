9b25d3717e2865c7a2466996688319a999b37ec2
phoenix mandala <phoenix.mandala@pm.me>
2025-04-20 20:27:45 -0500

    fixup ci setup

--- scripts/cargo-setup.py
+++ scripts/cargo-setup.py
@@ -5,12 +5,10 @@ import os
 import shutil
 import subprocess
 
+thispath = os.path.dirname(__file__)
+
 parser = argparse.ArgumentParser()
 
-parser.add_argument(
-    "-x", "--xwin", action="store_true", help="Install and splat xwin cache."
-)
-parser.add_argument("-b", "--bin", action="store_true", help="Link binaries.")
 parser.add_argument(
     "-D",
     "--no-deps",
@@ -33,16 +31,15 @@ if not args.no_deps:
         shell=True,
     )
 
-if args.xwin and (args.force or not os.path.exists(".xwin-cache")):
-    try:
-        os.rmdir(".xwin")
-    except:
-        pass
+xwin_path=os.path.abspath(os.path.join(thispath, "..", ".xwin-cache"))
+if args.force or not os.path.exists(xwin_path):
+    shutil.rmtree(xwin_path, ignore_errors=True)
     subprocess.run(["cargo", "install", "xwin", "--locked"])
     subprocess.run(["xwin", "--accept-license", "splat"])
 
-if args.bin and (args.force or not os.path.exists(".bin")):
-    shutil.rmtree(".bin", ignore_errors=True)
+bin_path = os.path.abspath(os.path.join(thispath, "..",".bin"))
+if args.force or not os.path.exists(bin_path):
+    shutil.rmtree(bin_path, ignore_errors=True)
     clang = shutil.which("clang-cl") or shutil.which("clang")
     llvm_ar = shutil.which("llvm-lib") or shutil.which("llvm-ar")
     lld_link = shutil.which("lld-link")
@@ -53,13 +50,13 @@ if args.bin and (args.force or not os.path.exists(".bin")):
     if not lld_link:
         print("WARN: Cannot find lld_link!")
 
-    os.mkdir(".bin")
-    os.symlink(f"{clang}", ".bin/clang-cl")
-    os.symlink(f"{llvm_ar}", ".bin/llvm-lib")
-    os.symlink(f"{lld_link}", ".bin/lld-link")
-    subprocess.check_call(["./.bin/clang-cl", "-v"])
-    subprocess.check_call(["./.bin/llvm-lib", "-v"])
-    subprocess.check_call(["./.bin/lld-link", "--version"])
+    os.mkdir(bin_path)
+    os.symlink(f"{clang}", f"{bin_path}/clang-cl")
+    os.symlink(f"{llvm_ar}", f"{bin_path}/llvm-lib")
+    os.symlink(f"{lld_link}",f"{bin_path}/lld-link")
+    subprocess.check_call([f"{bin_path}/clang-cl", "-v"])
+    subprocess.check_call([f"{bin_path}/llvm-lib", "-v"])
+    subprocess.check_call([f"{bin_path}/lld-link", "--version"])
 
 IMPORTANT = "\x1b[1m!IMPORTANT!\x1b[22m"
 print(
