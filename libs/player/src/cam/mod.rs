ed426667585ff63118057aeb12b34deae3832356
ada mandala <phoenix.mandala@pm.me>
2025-07-11 15:53:56 -0500

    fixup player services

--- libs/player/src/cam/mod.rs
+++ libs/player/src/cam/mod.rs
@@ -5,59 +5,80 @@ mod bundle;
 mod driver;
 pub use bundle::*;
 pub use driver::*;
-use q_utils::service;
+use q_service::prelude::*;
+use tiny_bail::prelude::*;
 
 use crate::prelude::*;
 use bevy::prelude::*;
 use bevy_dolly::prelude::*;
 
-service!(PlayerCam);
+#[derive(Debug, Clone, thiserror::Error)]
+pub enum PlayerCamError {
+    #[error("No player camera")]
+    NoCam,
+}
+
+pub fn update_camera(
+    mut set: ParamSet<(
+        Single<&Transform, With<Player>>,
+        Single<&mut Rig, With<PlayerCam>>,
+        Single<&mut Transform, With<PlayerCam>>,
+    )>,
+) {
+    let mut new_tf = **set.p0();
+    let lookat_pos = set.p0().translation;
+    new_tf.translation -= Vec3::new(0., -2., -10.);
+    new_tf.rotate_around(set.p0().translation, set.p0().rotation);
+    new_tf.look_at(set.p0().translation, Vec3::Y);
+    set.p1()
+        .driver_mut::<PlayerCamDriver>()
+        .set_target_position(new_tf.translation, new_tf.rotation, lookat_pos);
+    **set.p2() = new_tf;
+}
 
 pub struct PlayerCamPlugin;
-impl PlayerCamPlugin {
-    fn set_cam_active<const VAL: bool>(
-        mut cam: Single<&mut Camera, With<PlayerCam>>,
-    ) {
-        cam.is_active = VAL;
-    }
-
-    pub fn update_camera(
-        mut set: ParamSet<(
-            Single<&Transform, With<Player>>,
-            Single<&mut Rig, With<PlayerCam>>,
-            Single<&mut Transform, With<PlayerCam>>,
-        )>,
-    ) {
-        let mut new_tf = **set.p0();
-        let lookat_pos = set.p0().translation;
-        new_tf.translation -= Vec3::new(0., -2., -10.);
-        new_tf.rotate_around(set.p0().translation, set.p0().rotation);
-        new_tf.look_at(set.p0().translation, Vec3::Y);
-        set.p1()
-            .driver_mut::<PlayerCamDriver>()
-            .set_target_position(
-                new_tf.translation,
-                new_tf.rotation,
-                lookat_pos,
-            );
-        **set.p2() = new_tf;
-    }
-}
 impl Plugin for PlayerCamPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(PlayerCamServicePlugin)
-            .add_systems(
-                OnEnter(PlayerCamStates::Enabled),
-                Self::set_cam_active::<true>,
-            )
-            .add_systems(
-                OnEnter(PlayerCamStates::Disabled),
-                Self::set_cam_active::<false>,
-            )
-            .add_systems(
-                Update,
-                (Dolly::<PlayerCam>::update_active, Self::update_camera)
-                    .run_if(in_state(PlayerStates::Enabled)),
-            );
+        app.add_service(
+            SimpleServiceSpec::<PlayerCamError>::new("PlayerCam".into())
+                .is_startup(true)
+                .on_enable(set_cam_active::<true>)
+                .on_disable(set_cam_active::<true>),
+        );
+        app.add_systems(
+            Update,
+            (Dolly::<PlayerCam>::update_active, update_camera).run_if(
+                |services: Query<&SimpleService<PlayerError>>| {
+                    let s = r!(services.iter().find(|s| &s.name == "Player"));
+                    matches!(s.state, ServiceState::Enabled)
+                },
+            ),
+        );
     }
+}
+
+// pub const CAM_SERVICE_SPEC: ServiceSpec<PlayerServices> = ServiceSpec::<_> {
+//     name: PlayerServices::Cam,
+//     deps: vec![],
+//     is_startup: true,
+//     hooks: ServiceHooks {
+//         on_enable: set_cam_active::<true>,
+//         on_disable: set_cam_active::<false>,
+//         on_init: default_init,
+//         on_failure: default_fail,
+//     },
+// };
+
+// enable/disable hook
+fn set_cam_active<const VAL: bool>(
+    world: &mut World,
+) -> Result<(), PlayerCamError> {
+    let mut cam = r!(
+        Err(PlayerCamError::NoCam),
+        world
+            .query_filtered::<&mut Camera, With<PlayerCam>>()
+            .single_mut(world)
+    );
+    cam.is_active = VAL;
+    Ok(())
 }
--- libs/player/src/player/mod.rs
+++ libs/player/src/player/mod.rs
@@ -4,37 +4,44 @@
 mod bundle;
 pub use bundle::*;
 
-use bevy::prelude::*;
-use q_utils::service;
+use bevy::{ecs::system::RunSystemOnce, prelude::*};
+use q_service::prelude::*;
 
 use crate::prelude::*;
 
-service!(Player);
+#[derive(Debug, Clone, thiserror::Error)]
+pub enum PlayerError {}
 
 pub struct IntegrationPlugin;
-impl IntegrationPlugin {
-    // TODO: This position needs to vary depending on the terrain. Probably want
-    // to wait until it's loaded. But, game state should wait until terrain
-    // is loaded to transition to PlayerState::Enabled
-    fn spawn(
-        mut commands: Commands,
-        mut meshes: ResMut<Assets<Mesh>>,
-        mut materials: ResMut<Assets<StandardMaterial>>,
-    ) {
-        let pos = Vec3::ZERO;
-        let capsule = meshes.add(Capsule3d::new(0.5, 1.));
-        let material = materials.add(StandardMaterial::default());
-        commands.spawn(PlayerBundle::new(
-            Transform::from_translation(pos),
-            capsule,
-            material.clone(),
-        ));
-        commands.spawn(PlayerCamBundle::new());
-    }
-}
+impl IntegrationPlugin {}
 impl Plugin for IntegrationPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(PlayerServicePlugin)
-            .add_systems(OnEnter(PlayerStates::Initializing), Self::spawn);
+        app.add_service(
+            SimpleServiceSpec::<PlayerError>::new("Player".to_string())
+                .is_startup(true)
+                .on_init(|world| {
+                    world.run_system_once(spawn).unwrap();
+                    Ok(true)
+                }),
+        );
     }
+}
+
+// TODO: This position needs to vary depending on the terrain. Probably want
+// to wait until it's loaded. But, game state should wait until terrain
+// is loaded to transition to PlayerState::Enabled
+fn spawn(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let pos = Vec3::ZERO;
+    let capsule = meshes.add(Capsule3d::new(0.5, 1.));
+    let material = materials.add(StandardMaterial::default());
+    commands.spawn(PlayerBundle::new(
+        Transform::from_translation(pos),
+        capsule,
+        material.clone(),
+    ));
+    commands.spawn(PlayerCamBundle::new());
 }
